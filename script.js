a6ddf8f01e4c4a3886dd82505f56fd80d2804396
ttechcenter <92575777+ttechcenter@users.noreply.github.com>
2024-11-11 20:58:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,253 @@
+const ScrollRevealOption={
+    distance:"50px",
+    origin:"bottom",
+    duration:1000,
+ };
+ ScrollReveal().reveal(".nav",{
+    ...ScrollRevealOption,
+    origin:"right",
+ });
+ ScrollReveal().reveal(".header_image img",{
+     ...ScrollRevealOption,
+     origin:"left",
+     delay:200,
+ });
+ 
+ 
+ ScrollReveal().reveal(".header_content h1",{
+     ...ScrollRevealOption,
+     delay:500,
+ });
+ 
+ 
+ ScrollReveal().reveal(".header_content p",{
+     ...ScrollRevealOption,
+     delay:1000,
+ });
+ 
+ 
+ ScrollReveal().reveal(".header_content form",{
+     ...ScrollRevealOption,
+     delay:1500,
+ });
+ 
+ ScrollReveal().reveal(".nav_social",{
+    ...ScrollRevealOption,
+    delay:1600,
+});
+
+ 
+
+ function image1(){
+let image1=document.getElementById('image1');
+image1.src="img/oep.png";
+
+let image2=document.getElementById('image2');
+image2.src="img/oep.png";
+
+let image3=document.getElementById('image3');
+image3.src="img/oep.png";
+
+let image4=document.getElementById('image4');
+image4.src="img/oep.png";
+
+let image5=document.getElementById('image5');
+image5.src="img/oep.png";
+
+let detail=document.getElementById('detailp');
+detail.innerHTML="I'll upload details soon!";
+
+let language1=document.getElementById('languagep1');
+language1.innerHTML="Full Stack Website ";
+let language2=document.getElementById('languagep2');
+language2.innerHTML="I'll upload the language I used soon!";
+ }
+
+function image2(){
+    let image1=document.getElementById('image1');
+    image1.src="img/fed/pic1.jpg";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/fed/pic2.jpg";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/fed/pic3.jpg";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/fed/pic4.jpg";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/fed/pic5.jpg";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="This is a Facial Recognition system that will enable organization to make their system more modern regarding employees attendance, especially organization with high employees numbers."
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Full Stack Application ";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" Front End - Java JFrame"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Backend - Python, xampp server - phpmyadmin"
+}
+
+function image6(){
+    let image1=document.getElementById('image1');
+    image1.src="img/scl/Picture5.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/scl/Picture1.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/scl/Picture2.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/scl/Picture3.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/scl/Picture4.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="This is a school management application. That is designed to simplify the management in the institution."
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Java Application";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" Front End - Java JFrame"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Backend - xampp server - phpmyadmin"
+}
+
+function image4(){
+    let image1=document.getElementById('image1');
+    image1.src="img/per/Picture1.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/per/Picture2.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/per/Picture3.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/per/Picture4.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/per/Picture5.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="This is my personal portfolio website developed to show my profession skills and education background.  "
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Website";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" HTML,CSS,Javascript"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Link - "+ "<a>https://ttechcenter.github.io/temuofficial</a>";
+}
+
+function image7(){
+    let image1=document.getElementById('image1');
+    image1.src="img/preview.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/preview.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/preview.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/preview.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/preview.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="My Projects"
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Website";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" HTML,CSS,Javascript"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Link - "+ "<a>https://ttechcenter.github.io</a>";
+}
+function image8(){
+    let image1=document.getElementById('image1');
+    image1.src="img/preview.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/preview.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/preview.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/preview.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/preview.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="My Projects"
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Website";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" HTML,CSS,Javascript"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Link - "+ "<a>https://ttechcenter.github.io</a>";
+}
+function image9(){
+    let image1=document.getElementById('image1');
+    image1.src="img/preview.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/preview.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/preview.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/preview.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/preview.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="My Projects"
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Website";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" HTML,CSS,Javascript"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Link - "+ "<a>https://ttechcenter.github.io</a>";
+}
+function image10(){
+    let image1=document.getElementById('image1');
+    image1.src="img/preview.png";
+    
+    let image2=document.getElementById('image2');
+    image2.src="img/preview.png";
+    
+    let image3=document.getElementById('image3');
+    image3.src="img/preview.png";
+    
+    let image4=document.getElementById('image4');
+    image4.src="img/preview.png";
+    
+    let image5=document.getElementById('image5');
+    image5.src="img/preview.png";
+    
+    let detail=document.getElementById('detailp');
+    detail.innerHTML="My Projects"
+    
+    let language1=document.getElementById('languagep1');
+    language1.innerHTML="Website";
+    let language2=document.getElementById('languagep2');
+    language2.innerHTML=" HTML,CSS,Javascript"
+    let language3=document.getElementById('languagep3');
+    language3.innerHTML="Link - "+ "<a>https://ttechcenter.github.io</a>";
+}
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
